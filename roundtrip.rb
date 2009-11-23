cf69765f2cfd5ce353e2c45907ff60cfec11a053
Zhu Jia Cheng <realalien@gmail.com>
2009-11-23 18:31:14 +0800

    added new file as placeholder

---
+++ roundtrip.rb
@@ -0,0 +1,15 @@
+#  [EXPERIMENT]
+# Purpose: to automate the generation of UE3 script from design tools 
+# byproducts:
+#  * design tool for LDs auto generate target classes, logic 
+#  * parse existing code, sug. using syntax tree.
+#  * annotated code
+#  * accelerate the experiement/feedback from real world gadgets.
+#  *  
+
+
+
+
+
+
+
